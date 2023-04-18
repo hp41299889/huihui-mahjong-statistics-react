7d79013efd67b7993a2fc4113b8f31eec790e3a4
hp4129889 <hp4129889@gmail.com>
2023-04-18 08:57:07 +0800

    feat: add page

--- src/pages/player/PlayerSearch.tsx
+++ src/pages/player/PlayerSearch.tsx
@@ -1,10 +1,31 @@
+import { Button, Form, Input } from 'antd';
 import React from 'react';
+import { mahjongApi } from '../../utils/request';
 
 const PlayerSearch: React.FC = () => {
+    const onSubmit = (value: any) => {
+        console.log(value);
+        mahjongApi.get(`/player/${value.name}`)
+            .then((res) => {
+                console.log(res);
+
+            })
+
+    };
+
     return (
-        <>
-            Search Player
-        </>
+        <Form
+            onFinish={onSubmit}
+        >
+            <Form.Item name='name'>
+                <Input />
+            </Form.Item>
+            <Form.Item>
+                <Button htmlType='submit' type='primary'>
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
     )
 };
 
--- src/pages/record/WinningForm.tsx
+++ src/pages/record/WinningForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { Form, Radio, RadioChangeEvent } from "antd";
 import { EWind } from "../enum";
 import PointIntput from "./PointInput";
@@ -25,13 +25,23 @@ const WinningForm: React.FC<IWinningForm> = (props) => {
     };
 
     const winnerOptions = [
-        { label: windPlayerMap[EWind.EAST], value: EWind.EAST },
-        { label: windPlayerMap[EWind.SOUTH], value: EWind.SOUTH },
-        { label: windPlayerMap[EWind.WEST], value: EWind.WEST },
-        { label: windPlayerMap[EWind.NORTH], value: EWind.NORTH },
+        { label: windPlayerMap[EWind.EAST], value: players.east.name },
+        { label: windPlayerMap[EWind.SOUTH], value: players.south.name },
+        { label: windPlayerMap[EWind.WEST], value: players.west.name },
+        { label: windPlayerMap[EWind.NORTH], value: players.north.name },
     ];
 
     const loserOptions = winnerOptions.filter(option => option.value !== winner);
+
+    const renderLoserRadio = useMemo(() => {
+        return (
+            <Radio.Group
+                onChange={onChangeLoser}
+                value={loser}
+                options={loserOptions}
+            />
+        )
+    }, [loser, loserOptions]);
 
     return (
         <>
@@ -49,11 +59,7 @@ const WinningForm: React.FC<IWinningForm> = (props) => {
                 label={'放槍'}
                 name={'loser'}
             >
-                <Radio.Group
-                    onChange={onChangeLoser}
-                    value={loser}
-                    options={loserOptions}
-                />
+                {renderLoserRadio}
             </Form.Item>
             <PointIntput />
         </>
